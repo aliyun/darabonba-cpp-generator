4336f873c2c46fcaa1ffc83ff2db2af065c209bb
wb-hx510875 <wb-hx510875@alibaba-inc.com>
2020-09-27 15:22:03 +0800

    add fixed method for emitter

--- src/lib/emitter.js
+++ src/lib/emitter.js
@@ -28,6 +28,26 @@ class Emitter {
     return this.config.indent.repeat(level);
   }
 
+  fixed(content, length = 10, fillPosition = 'l', fill = ' ') {
+    if (content.length < length) {
+      var leftFill = '';
+      var rightFill = '';
+      if (fillPosition.indexOf('r') === 0) {
+        leftFill = fill.repeat(length - content.length);
+      } else if (fillPosition.indexOf('c') === 0) {
+        var left = Math.floor((length - content.length) / 2);
+        leftFill = fill.repeat(left);
+        rightFill = fill.repeat(length - content.length - left);
+      } else {
+        rightFill = fill.repeat(length - content.length);
+      }
+      fill = fill.repeat(length - content.length);
+      content = leftFill + content + rightFill;
+    }
+    this.emit(content);
+    return this;
+  }
+
   emit(str = '', level) {
     this.output += this.indent(level) + str;
     return this;

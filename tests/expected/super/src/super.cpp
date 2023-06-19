4db0841abe827b99fd9244135e982e2f9160a284
page <page@B-KY0R7MCQ-1143.local>
2023-06-19 19:26:00 +0800

    change cpp generator to 2.x

--- tests/expected/super/src/super.cpp
+++
@@ -1,8 +0,0 @@
-// This file is auto-generated, don't edit it. Thanks.
-
-#include <darabonba/super.hpp>
-#include <darabonba/import.hpp>
-
-using namespace Darabonba_Super;
-
-Darabonba_Super::Client::Client(const shared_ptr<Darabonba_Import::Config>& config) : Darabonba_Import::Client(config) {};
